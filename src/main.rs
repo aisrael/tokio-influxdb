7caf7ea47146636c69cc23f5f3a30ad94aaac1d4
Alistair A. Israel <aisrael@gmail.com>
2019-09-24 11:08:50 +0800

    This works

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use futures::future::lazy;
+use futures::future::*;
 use influxdb::client::InfluxDbClient;
 use influxdb::query::{InfluxDbQuery, Timestamp};
 
@@ -8,5 +8,13 @@ fn main() {
     let write_query =
         InfluxDbQuery::write_query(Timestamp::NOW, "weather").add_field("temperature", 82);
 
-    tokio::spawn(client.query(&write_query));
+    tokio::spawn(
+        lazy(move || client.query(&write_query))
+            .map(|x| {
+                println!("{:?}", &x);
+            })
+            .map_err(|e| {
+                println!("{:?}", &e);
+            }),
+    );
 }
